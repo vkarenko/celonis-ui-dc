6a98e39121012fe4f6c165fc416a162821234d73
vkarenko <vitalii.karenko@me.com>
2020-01-24 17:14:41 +0100

    initiate app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { NavigationComponent } from './layout/sidebar/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
